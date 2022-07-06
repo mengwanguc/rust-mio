a3bef87495012bd6d44c00dc7193f6ece70b3374
Meng Wang <mengwanguc@gmail.com>
2022-07-06 01:08:31 +0000

    update tests

--- src/client.rs
+++ src/client.rs
@@ -67,6 +67,11 @@ mod tests {
     use super::*;
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+    static ha_addr_str: &str = "inet:tcp:10.140.82.80@22001";
+    static local_addr_str: &str = "inet:tcp:10.140.82.80@22501";
+    static profile_fid_str: &str = "0x7000000000000001:0x0";
+    static process_fid_str: &str = "0x7200000000000001:0x3";
+
     pub fn str_to_c_char(v: Vec<u8>) -> *mut c_char {
         let bytes: Vec<u8> = v;
         let mut c_chars: Vec<i8> = bytes.iter().map(| c | *c as i8).collect::<Vec<i8>>();
@@ -80,15 +85,15 @@ mod tests {
     #[test]
     fn test_read() {
         unsafe {
-            let ha_addr = CString::new("172.31.12.177@tcp:12345:34:1").expect("CString::new failed");
-            let local_addr = CString::new("172.31.12.177@tcp:12345:33:1000").expect("CString::new failed");
-            let profile_fid = CString::new("0x7000000000000001:0").expect("CString::new failed");
-            let process_fid = CString::new("0x7200000000000001:64").expect("CString::new failed");
+            let ha_addr = CString::new(ha_addr_str).expect("CString::new failed");
+            let local_addr = CString::new(local_addr_str).expect("CString::new failed");
+            let profile_fid = CString::new(profile_fid_str).expect("CString::new failed");
+            let process_fid = CString::new(process_fid_str).expect("CString::new failed");
             let client = MotrClient::new(ha_addr.as_ptr(), local_addr.as_ptr(), 
                                          profile_fid.as_ptr(), process_fid.as_ptr());
             println!("\nreadingnow\n\n\n");
 
-            let rres = client.read_object(12345, 9999999999, 4099, 8188);
+            let rres = client.read_object(12345, 9999999999, 400, 5000);
             println!("done reading\n\n\n");
 
             let res_bytes = slice::from_raw_parts((*rres).data as *const u8, (*rres).len as _);
@@ -101,16 +106,16 @@ mod tests {
     #[test]
     fn test_create() {
         unsafe {
-            let ha_addr = CString::new("172.31.12.177@tcp:12345:34:1").expect("CString::new failed");
-            let local_addr = CString::new("172.31.12.177@tcp:12345:33:1000").expect("CString::new failed");
-            let profile_fid = CString::new("0x7000000000000001:0").expect("CString::new failed");
-            let process_fid = CString::new("0x7200000000000001:64").expect("CString::new failed");
+            let ha_addr = CString::new(ha_addr_str).expect("CString::new failed");
+            let local_addr = CString::new(local_addr_str).expect("CString::new failed");
+            let profile_fid = CString::new(profile_fid_str).expect("CString::new failed");
+            let process_fid = CString::new(process_fid_str).expect("CString::new failed");
             let client = MotrClient::new(ha_addr.as_ptr(), local_addr.as_ptr(), 
                                          profile_fid.as_ptr(), process_fid.as_ptr());
             println!("\ncreatingingnow\n\n\n");
 
             let rres = client.create_object(12345, 9999999999);
-            println!("done creading  res:{}\n\n\n", rres);        
+            println!("done creating  res:{}\n\n\n", rres);        
 
         }
     }
@@ -118,16 +123,16 @@ mod tests {
     #[test]
     fn test_write() {
         unsafe {
-            let ha_addr = CString::new("172.31.12.177@tcp:12345:34:1").expect("CString::new failed");
-            let local_addr = CString::new("172.31.12.177@tcp:12345:33:1000").expect("CString::new failed");
-            let profile_fid = CString::new("0x7000000000000001:0").expect("CString::new failed");
-            let process_fid = CString::new("0x7200000000000001:64").expect("CString::new failed");
+            let ha_addr = CString::new(ha_addr_str).expect("CString::new failed");
+            let local_addr = CString::new(local_addr_str).expect("CString::new failed");
+            let profile_fid = CString::new(profile_fid_str).expect("CString::new failed");
+            let process_fid = CString::new(process_fid_str).expect("CString::new failed");
             let client = MotrClient::new(ha_addr.as_ptr(), local_addr.as_ptr(), 
                                          profile_fid.as_ptr(), process_fid.as_ptr());
-            println!("\ncreatingingnow\n\n\n");
+            println!("\nwriting now\n\n\n");
 
             let rres = client.write_object(12345, 9999999999, 0, 8192, str_to_c_char(vec![b'X'; 8192]));
-            println!("done creading  res:{}\n\n\n", rres);        
+            println!("done writing  res:{}\n\n\n", rres);        
 
         }
     }
